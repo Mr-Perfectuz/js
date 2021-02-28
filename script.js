4e4d485e88ea440174005eab5af123122238c244
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-02-28 23:51:02 +0500

    Updated

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log(4534);
 alert("Watch Out ");
+
+//String
+
+const streetName = "President St";
+const houseNumber = 153;
+const tenant =  "John Doe";
+
+console.log(`${tenant} lives in ${streetName} at house number ${houseNumber}`);
+
+console.log(tenant.split(' '));
